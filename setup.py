a5485417fba7f7e9d94782bebbf83fcb64d1e8c5
Thiago de Arruda <tpadilha84@gmail.com>
2017-12-30 16:07:20 -0300

    Bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='ush',
   py_modules=['ush'],
-  version='2.0.0',
+  version='2.1.0',
   description='Powerful API for invoking with external commands',
   author='Thiago de Arruda',
   author_email='tpadilha84@gmail.com',
