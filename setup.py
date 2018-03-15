9f6e5497a0e727122cfc2f94642ea0bc888ff859
Thiago de Arruda <tpadilha84@gmail.com>
2018-03-15 18:51:55 -0300

    Bump

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+
+VERSION = '3.0.0'
+REPO    = 'https://github.com/tarruda/python-ush'
+
 setup(
   name='ush',
   py_modules=['ush'],
-  version='2.1.0',
+  version=VERSION,
   description='Powerful API for invoking with external commands',
   author='Thiago de Arruda',
   author_email='tpadilha84@gmail.com',
   license='MIT',
-  url='https://github.com/tarruda/python-ush',
-  download_url='https://github.com/tarruda/python-ush/archive/master.tar.gz',
+  url=REPO,
+  download_url='{0}/archive/{1}.tar.gz'.format(REPO, VERSION),
   keywords=['sh', 'unix', 'bash', 'shell', 'glob']
 )
